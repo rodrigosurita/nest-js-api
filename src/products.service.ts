188cfad0e3fc3d6e301acd25ea2c085e41d35531
Rodrigo Surita <rodrigosurita@live.com>
2021-01-30 20:37:18 -0300

    Chore: CRUD methods added to products service

--- src/products.service.ts
+++ src/products.service.ts
@@ -6,4 +6,24 @@ export class ProductsService {
         new Product('B02', 'AI Book', 8.90),
         new Product('B03', 'TS Book', 39.90)
     ];
+
+    getAll(): Product[]{
+        return this.products;
+    }
+
+    getOne(id: number): Product{
+        return this.products[0];
+    }
+
+    create(product: Product) {
+        this.products.push(product);
+    }
+
+    alter(product: Product): Product{
+        return product;
+    }
+
+    delete(id: number) {
+        this.products.pop();
+    }
 }
