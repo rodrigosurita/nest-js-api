d12c155393e70ad5042f1051daa845c05198a1d8
Rodrigo Surita <rodrigosurita@live.com>
2021-01-30 20:02:30 -0300

    Feature: alter product, create product, get one product and delete product added to the api

--- src/products.controle.ts
+++ src/products.controle.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
 @Controller('products')
 export class ProductsController {
@@ -6,6 +6,30 @@ export class ProductsController {
     @Get()
     getAll(): string{
         return 'list all products';
+    }
+
+    @Get(':id')
+    getOne(@Param() params): string{
+        return `return data from product ${params.id}`;
+    }
+
+    @Post()
+    create(@Body() product): string{
+        console.log(product);
+        
+        return 'product created';
+    }
+
+    @Put()
+    alter(@Body() product): string{
+        console.log(product);
+        
+        return 'product updated';
+    }
+
+    @Delete(':id')
+    delete(@Param() params): string{
+        return `delete product ${params.id}`;
     }
 
 }
